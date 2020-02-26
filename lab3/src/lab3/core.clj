8a7dace008a7203b2b87cf26b98dc96520ea1c56
PauLOk777 <trocjuk2000@gmail.com>
2020-02-26 18:05:15 +0200

    Lab3 was implemented.

--- lab3/src/lab3/core.clj
+++ lab3/src/lab3/core.clj
@@ -1,42 +1,80 @@
 (ns lab3.core
   (:gen-class)
-  (:require [clojure.string :as str]))
+  (:use [clojure.data.csv])
+  (:require [clojure.string :as str]
+            [clojure.data.csv :as csv]
+            [clojure.java.io :as io]))
+
 
 (defn getFile [name]
   (def string1 (slurp name))
   string1)
 
-(defn makeTableCSV [name]
-  (map #(clojure.string/split % #",")
+(defn makeTableTSV [name]
+  (map #(clojure.string/split % "\t")
        (clojure.string/split (getFile name) #"\n")))
 
-(defn printTableCSV [table]
+(defn makeTableCSV [name]
+  (with-open [reader (io/reader name)]
+    (doall
+      (csv/read-csv reader))))
+
+(defn printTable [table]
   (loop [i 0]
     (when (< i (count table))
       (loop [j 0]
         (when (< j (count (nth table i)))
-          (print (format "%35s|" (nth (nth table i) j)))
+          (print (format "%40s| " (nth (nth table i) j)))
           (recur (+ j 1))))
-      (println "\n")
+      (println "")
       (recur (+ i 1)))))
 
 (defn checkFormat [name]
-  (def formatFile (clojure.string/split name #"\."))
+  (def formatFile (str/split name #"\."))
   (if (= (str (nth formatFile 1)) "csv") true false))
 
 (defn -main
   [& args]
   (println "Input name of file: ")
   (def input (read-line))
-  ;(def input "mp-posts_full.csv")
+  ;(def input "mp-assistants.csv")
   (if (checkFormat input)
-    (printTableCSV (makeTableCSV input))
-    (println "Hello")))
+    (printTable (makeTableCSV input))
+    (printTable (makeTableTSV input))))
 
+;(-main)
 ;(getRegexp 3)
 ;
 ;(loop [x 0]
 ;  (when (< x 5)
-;    (println (format "%s|%s" "daun" "eblan"))
+;    (println (format "%s|%s" "123" "4566"))
 ;    (recur (+ x 1))))
 ;(format "%s" (nth [1 "dd"] 1))
+;
+;(.indexOf ["t" "v" "d"] "d")
+;
+;(with-open [reader (clojure.java.io/reader "mp-posts_full.csv")]
+;  (doall
+;    (csv/read-csv reader)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
