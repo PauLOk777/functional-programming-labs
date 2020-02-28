97ef8bf59844d875e705de6aa649043d8c18358d
PauLOk777 <trocjuk2000@gmail.com>
2020-02-29 00:10:18 +0200

    1/3 part of lab4 was done. Now we can select so many columns from 1 table and use * for select all of them.

--- lab4-7/src/lab4_7/core.clj
+++ lab4-7/src/lab4_7/core.clj
@@ -23,19 +23,31 @@
 (defn makeTableCSV [name]
   (apply data->maps (readCSV name)))
 
+(defn getKeysForPrint [numTable table]
+  (keys (first (nth table numTable))))
+
+(defn getValsForPrint [numTable numRow table]
+  (vals (nth (nth table numTable) numRow)))
+
 (defn printTable [table]
-  (loop [k 0]
-    (when (< k (count (first table)))
-      (print (format "%40s| " (name (nth (keys (first table)) k))))
-      (recur (+ k 1))))
+  (loop [l 0]
+    (when (< l (count table))
+    (loop [k 0]
+      (when (< k (count (first (nth table l))))
+        (print (format "%40s| " (name (nth (getKeysForPrint l table) k))))
+        (recur (+ k 1))))
+    (recur (+ l 1))))
   (println)
   (loop [i 0]
-    (when (< i (count table))
+    (when (< i (count (first table)))
       (loop [j 0]
-        (when (< j (count (nth table i)))
-          (print (format "%40s| " (nth (vals (nth table i)) j)))
-          (recur (+ j 1))))
-      (println "")
+        (when (< j (count table))
+            (loop [z 0]
+              (when (< z (count (nth (nth table j) i)))
+                (print (format "%40s| " (nth (getValsForPrint j i table) z)))
+                (recur (+ z 1))))
+            (recur (+ j 1))))
+      (println)
       (recur (+ i 1)))))
 
 (defn checkFormat [name]
@@ -47,27 +59,32 @@
     (makeTableCSV name)
     (makeTableTSV name)))
 
+(defn getColumn [columnName file]
+  (map #(select-keys % [(keyword columnName)]) file))
+
 (defn getMainTable [columns fileName]
   (def file (getFile fileName))
-  file)
+    (map #(if (not= 0 (compare % "*"))
+            (getColumn % file)
+            file) columns))
 
 (defn withDistinct [expr commands]
   [])
 
 (defn noDistinct [words commands]
   (cond
-    (not= 0 (compare (nth words 2) (nth commands 1))) []
+    (not= 0 (compare (str/upper-case (nth words 2)) (nth commands 1))) []
     :else
     (getMainTable (str/split (nth words 1) #",") (nth words 3))))
 
 (defn processColumns [words commands]
-  (if (not= 0 (compare (nth words 1) (nth commands 3)))
+  (if (not= 0 (compare (str/upper-case (nth words 1)) (nth commands 3)))
     (noDistinct words commands)
     (withDistinct words commands)))
 
 (defn getResult [expr commands]
   (def words (str/split expr #"\s"))
-  (if (not= 0 (compare (first words) (first commands))) []
+  (if (not= 0 (compare (str/upper-case (first words)) (first commands))) []
     (processColumns words commands)))
 
 (defn -main
@@ -75,6 +92,4 @@
   (println "Write your query below this: ")
   (def input (read-line))
   (def commands ["SELECT" "FROM" "WHERE" "DISTINCT"])
-  (getResult input commands))
-
-(-main)
+  (printTable (getResult input commands)))
